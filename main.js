3cd89c568a830dced55ef40d7bab43e4054edaa8
KaptainKiwi <Sethpalmer3@gmail.com>
2017-07-25 15:36:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-$(document).ready(function() {
-	$(".open-icon").click(function() {
-		$(".menu").animate({
-			left: '0px'
-		}, 200);
+menubutton = document.getElementsByClassName("open-icon")[0];
+menuscreen = document.getElementsByClassName("menu")[0];
 
-		$("body").animate({
-			left: '285px'
-		}, 200);
-	});
+function openClose() {
+	mbpos = paseInt(menubutton.style.left.slice(0,(menubutton.style.left.length - 2)));
+	mspos = paseInt(menuscreen.style.left.slice(0,(menuscreen.style.left.length - 2)));
+	if(menuscreen.style.left == -258){
+		var myvar = setInterval("click", function(){
+			if(menuscreen.style.left == 0){
+				clearInterval(myvar);
+			}
+			else{
+				menubutton.style.left += 5;
+				menuscreen.style.left += 5;
+			}
+		},10);
+	else{
+		var myvar = setInterval("click", function(){
+			if(menuscreen.style.left == -285){
+				clearInterval(myvar);
+			}
+			else{
+				menubutton.style.left -= 5;
+				menuscreen.style.left -= 5;
+			}
+		},10);
+	}
+	
+	}
 
-	$(".close-icon").click(function() {
-		$(".menu").animate({
-			left: '-285px'
-		},200);
-
-		$("body").animate({
-			left: '0px'
-		},200);
-	});
-});
+}
