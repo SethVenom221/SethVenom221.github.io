570385aad1e457ced1507d06eeca45ef4e0ad3ef
KaptainKiwi <Sethpalmer3@gmail.com>
2018-12-19 09:55:49 -0800

    Delete board.js

--- board.js
+++
@@ -1,9 +0,0 @@
-var board = document.createElement("canvas");
-board.width = window.innerWidth;
-board.height = window.innerHeight - 50;
-board.style.width = board.width + "px";
-board.style.height = board.height + "px";
-board.style.position = "absolute";
-board.style.left = "0px";
-board.style.top = "50px";
-document.body.appendChild(board);
