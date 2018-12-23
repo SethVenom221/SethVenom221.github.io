49b0dd261841565cf6f664b233a9f55f937226a3
KaptainKiwi <Sethpalmer3@gmail.com>
2018-12-23 10:53:32 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 var config = {
   API_KEY:'AIzaSyBH5atiGBTpWOKnnLIw7MxkHwM-ZQesuG8',
-  CLIENT_ID:'867106802265-eekh6ha7hbq72khlvka44rnm0ium3mrd.apps.googleusercontent.com'
+  CLIENT_ID:'867106802265-eekh6ha7hbq72khlvka44rnm0ium3mrd.apps.googleusercontent.com',
+  API_KEY2:'AIzaSyD7ySX9XR5vlSZmqb6I5P-0YIIljHmtU6E',
+  CLIENT_ID2:'323222569022-9i8q4sou3k46dqk7auevj22rd0n0s94a.apps.googleusercontent.com'
 }
